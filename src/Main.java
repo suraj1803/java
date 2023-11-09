ffb49a5311f92d94c014156119e09506a06274c1
Suraj Biswas <surajbiswas182003@gmail.com>
2023-11-09 23:46:32 +0530

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,5 @@ public class Main {
     public static void main(String[] args) {
         GuessGame guessGame = new GuessGame();
         guessGame.startGame();
-//        int num = (int)(Math.random() * 100 + 1);
-//        System.out.println(num);
     }
 }
--- src/game/GuessGame.java
+++ src/game/GuessGame.java
@@ -1,9 +1,53 @@
 package game;
 
+import java.util.Scanner;
+
 public class GuessGame {
     Player player1;
     Player player2;
     Player player3;
+
+    public void startGame() {
+        player1 = new Player();
+        player2 = new Player();
+        player3 = new Player();
+
+        while (true) {
+            int secretNumber = (int) (100 * Math.random() + 1);
+
+            System.out.print("Player 1 is guessing: ");
+            player1.guess();
+            System.out.print("Player 2 is guessing: ");
+            player2.guess();
+            System.out.print("Player 3 is guessing: ");
+            player3.guess();
+
+            int distance1 = distance(secretNumber, player1.number);
+            int distance2 = distance(secretNumber, player2.number);
+            int distance3 = distance(secretNumber, player3.number);
+
+            int smallestDistance = min(distance1, distance2, distance3);
+
+            if (smallestDistance == distance1)
+                System.out.println("Player 1 wins!");
+            else if (smallestDistance == distance2)
+                System.out.println("Player 2 wins!");
+            else
+                System.out.println("Player 3 wins!");
+
+            System.out.println("Game is over.");
+            System.out.println("The secret number was " + secretNumber);
+
+            System.out.print("Enter a to play again and enter q to quit: ");
+            Scanner scanner = new Scanner(System.in);
+            String ch = scanner.next();
+
+            if (ch.equals("a"))
+                continue;
+            else
+                break;
+        }
+    }
 
     public int min(int a, int b, int c) {
         return Math.min(Math.min(a, b), c);
@@ -11,36 +55,5 @@ public class GuessGame {
 
     public int distance(int secretNumber, int num) {
         return (secretNumber >= num) ? secretNumber - num : num - secretNumber;
-    }
-
-    public void startGame() {
-        player1 = new Player();
-        player2 = new Player();
-        player3 = new Player();
-
-        int secretNumber = (int) (100 * Math.random() + 1);
-
-        System.out.print("Player 1 is guessing: ");
-        player1.guess();
-        System.out.print("Player 2 is guessing: ");
-        player2.guess();
-        System.out.print("Player 3 is guessing: ");
-        player3.guess();
-
-        int distance1 = distance(secretNumber, player1.number);
-        int distance2 = distance(secretNumber, player2.number);
-        int distance3 = distance(secretNumber, player3.number);
-
-        int smallestDistance = min(distance1, distance2, distance3);
-
-        if (smallestDistance == distance1)
-            System.out.println("Player 1 wins!");
-        else if (smallestDistance == distance2)
-            System.out.println("Player 2 wins!");
-        else
-            System.out.println("Player 3 wins!");
-
-        System.out.println("Game is over.");
-        System.out.println("The secret number was " + secretNumber);
     }
 }
